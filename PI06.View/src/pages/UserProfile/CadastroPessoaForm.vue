6f01cdaa907ac10ed9acc41ca4f4cbdd076a28ba
EdsonJunioMalaquias <30880842+EdsonJunioMalaquias@users.noreply.github.com>
2019-11-27 15:13:56 -0300

    correção cadastro Pessoa

--- PI06.View/src/pages/UserProfile/CadastroPessoaForm.vue
+++ PI06.View/src/pages/UserProfile/CadastroPessoaForm.vue
@@ -9,8 +9,8 @@
         <md-card-content>
           <div class="md-layout">
             <div class="md-layout-item md-small-size-100 md-size-100">
-                <br>
-                <h4 class="title">Dados Pessoais</h4>
+              <br />
+              <h4 class="title">Dados Pessoais</h4>
             </div>
             <div class="md-layout-item md-small-size-100 md-size-40">
               <md-field>
@@ -62,7 +62,7 @@
             <div class="md-layout-item md-small-size-100 md-size-23">
               <md-field>
                 <label>Bairro</label>
-                <md-input v-model="complemento" type="text"></md-input>
+                <md-input v-model="bairro" type="text"></md-input>
               </md-field>
             </div>
             <div class="md-layout-item md-small-size-100 md-size-33">
@@ -99,16 +99,14 @@
                 <md-input v-model="numeroTelefone" type="number"></md-input>
               </md-field>
             </div>
-            <div class="md-layout-item md-small-size-100 md-size-40">
-      
-            </div>
+            <div class="md-layout-item md-small-size-100 md-size-40"></div>
             <div class="md-layout">
               <div class="md-layout-item md-small-size-100 md-size-100">
                 <div v-if="isFuncionario">
                   <md-switch class="switch" v-model="isFuncionario">Cadastrar Funcionario</md-switch>
                 </div>
                 <div v-else>
-                    <md-switch class="switch" v-model="isFuncionario">Cadastrar Paciente</md-switch>
+                  <md-switch class="switch" v-model="isFuncionario">Cadastrar Paciente</md-switch>
                 </div>
               </div>
               <div v-if="isFuncionario" class="md-layout-item md-small-size-100 md-size-30">
@@ -116,8 +114,9 @@
                   <label>Cargo</label>
                 </md-autocomplete>
               </div>
-              <div 
-                class="md-layout" v-if="selectCargo != null && selectCargo != ''&& selectCargo !='Recepcionista'"
+              <div
+                class="md-layout"
+                v-if="selectCargo != null && selectCargo != ''&& selectCargo !='Recepcionista'"
               >
                 <div v-if="isFuncionario" class="md-layout-item md-small-size-100 md-size-30">
                   <md-field>
@@ -135,25 +134,41 @@
             </div>
             <div class="md-layout-item md-size-100 text-right">
               <md-button @click="cadastrar()" class="md-raised md-success">Novo Cadastro</md-button>
-            </div>          
+            </div>
           </div>
         </md-card-content>
       </md-card>
+      <md-snackbar
+        md-position="center"
+        :md-duration="duracao"
+        :md-active.sync="showSnackbar"
+        md-persistent
+      >
+        <span>{{ error }}</span>
+        <md-button class="md-primary" @click="showSnackbar = false">Fechar</md-button>
+      </md-snackbar>
     </div>
   </div>
 </template>
 
 <script>
+import funcionario from "../../../services/funcionario";
+import paciente from "../../../services/paciente";
+
+import moment from "moment";
 export default {
   name: "cadastro-pessoa-form",
   data: () => ({
-    obj1: {name: 'obj1'},
-    obj2: {name: 'obj2'},
+    obj1: { name: "obj1" },
+    obj2: { name: "obj2" },
     isFuncionario: false,
-    isCargoSaude: false,
     selectedCidade: null,
     selectedUF: null,
     selectCargo: null,
+
+    showSnackbar: false,
+    error: "Erro Inesperado!",
+    duracao: 4000,
 
     nome: "",
     dataNascimento: null,
@@ -163,6 +178,7 @@ export default {
     logradouro: "",
     complemento: "",
     numeroResidencia: "",
+    bairro: "",
     cep: "",
     sus: "",
     ddd: "",
@@ -182,19 +198,54 @@ export default {
     cargo: ["Medico", "Recepcionista", "Enfermeiro"]
   }),
   methods: {
-    Cadastrar() {
-      let pessoa = this.cadastrarPessoa;
-      console.log(pessoa);
-      if (this.isFuncionario) {
-        this.cadastrarFuncionario(pessoa);
-      } else {
-        this.CadastrarPaciente(pessoa);
-      }
+    recebeDateTimeERetornaSomenteODate(dateTime) {
+      return moment(dateTime).format("YYYY-MM-DD");
     },
-    cadastrarPessoa() {
-      let obj = {
+    validador() {
+      if (
+        this.nome == null ||
+        this.nome == "" ||
+        this.dataNascimento == null ||
+        this.dataNascimento == "" ||
+        this.rg == null ||
+        this.rg == "" ||
+        this.sus == null ||
+        this.sus == "" ||
+        this.cpf == null ||
+        this.cpf == "" ||
+        this.enderecoEmail == null ||
+        this.enderecoEmail == "" ||
+        this.logradouro == null ||
+        this.logradouro == "" ||
+        this.numeroResidencia == null ||
+        this.numeroResidencia == "" ||
+        this.bairro == null ||
+        this.bairro == "" ||
+        this.selectedCidade == null ||
+        this.selectedCidade == "" ||
+        this.selectedUF == null ||
+        this.selectedUF == "" ||
+        this.numeroTelefone == null ||
+        this.numeroTelefone == "" ||
+        this.ddd == null ||
+        this.ddd == "" ||
+        this.cep == null ||
+        this.cep == ""
+      ) {
+        return false;
+      }
+      return true;
+    },
+    cadastrar() {
+      console.log(this.recebeDateTimeERetornaSomenteODate(this.dataNascimento));
+      if (!this.validador()) {
+        this.error = "Preencha todos os campos!";
+        this.showSnackbar = true;
+        return;
+      }
+      var pessoa = {
         nome: this.nome,
-        dataNascimento: this.dataNascimento,
+        dataNascimento: this.recebeDateTimeERetornaSomenteODate(this.dataNascimento),
         rg: this.rg,
         sus: this.sus,
         codigoCpf: this.cpf,
@@ -204,19 +255,95 @@ export default {
         numeroEndereco: this.numeroResidencia,
         bairro: this.bairro,
         cidade: this.selectedCidade,
-        uf: this.selectedUF,
+        uf: 13,
         numeroTelefone: this.numeroTelefone,
         ddd: this.ddd,
-        cep: this.cep
+        cepCod: this.cep
       };
-      return obj;
+      
+      if (this.isFuncionario) {
+        this.cadastrarFuncionario(pessoa);
+      } else {
+        this.CadastrarPaciente(pessoa);
+      }
     },
     CadastrarPaciente(obj) {
-      //this.$set(this.someObject, 'b', 2)
-      console.log(obj);
+      var pessoaPaciente = {pessoa:{}}
+      Object.assign(pessoaPaciente.pessoa, obj);
+      paciente
+        .post(pessoaPaciente)
+        .then(res => {
+          this.error = "Cadastro efetuado com sucesso!";
+          this.showSnackbar = true;
+        })
+        .catch(err => {
+          this.error = err.message;
+          this.showSnackbar = true;
+        });
     },
+    		
     cadastrarFuncionario(obj) {
-      console.log(obj);
+      var pessoaFuncionario = {pessoa:{}}
+      Object.assign(teste3.pessoa, obj);
+      if (this.selectCargo == null || this.selectCargo == "") {
+        this.error = "Selecione um cargo valido!";
+        this.showSnackbar = true;
+        return;
+      }
+      if (this.selectCargo == "Medico") {
+        if (
+          this.numeroConselho == null ||
+          this.numeroConselho == "" ||
+          this.descricaoConselho == null ||
+          this.descricaoConselho == ""
+        ) {
+          this.error = "Preencha a descrição do conselho e o número!";
+          this.showSnackbar = true;
+          return;
+        }
+
+        Object.assign(obj, {
+          conselho: {
+            numeroConselho: this.numeroConselho,
+            descricaoConselho: this.descricaoConselho
+          }
+        });
+        this.$set(obj, "cargoId", 5);
+      } else if (this.selectCargo == "Enfermeiro") {
+        if (
+          this.numeroConselho == null ||
+          this.numeroConselho == "" ||
+          this.descricaoConselho == null ||
+          this.descricaoConselho == ""
+        ) {
+          this.error = "Preencha a descrição do conselho e o número!";
+          this.showSnackbar = true;
+          return;
+        }
+
+        Object.assign(obj, {
+          conselho: {
+            numeroConselho: this.numeroConselho,
+            descricaoConselho: this.descricaoConselho
+          }
+        });
+        this.$set(obj, "cargoId", 6);
+      } else if (this.selectCargo == "Recepcionista") {
+        this.$set(obj, "cargoId", 7);
+      }
+
+
+      return;
+      funcionario
+        .post(obj)
+        .then(res => {
+          this.error = "Cadastro efetuado com sucesso!";
+          this.showSnackbar = true;
+        })
+        .catch(err => {
+          this.error = err.message;
+          this.showSnackbar = true;
+        });
     }
   }
 };
